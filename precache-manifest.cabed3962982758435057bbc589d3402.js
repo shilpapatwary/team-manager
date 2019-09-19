d949789e443b46141ade6004941627abb4b4964f
Shilpa Patwary <shilpa.patwary@sapient.com>
2019-09-19 16:41:36 +0530

    Updates

---
+++ precache-manifest.cabed3962982758435057bbc589d3402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59f89c756a22e81835a6",
+    "url": "/team-manager/static/js/runtime~main.59f89c75.js"
+  },
+  {
+    "revision": "41b21d2929a87aa299c1",
+    "url": "/team-manager/static/js/main.41b21d29.chunk.js"
+  },
+  {
+    "revision": "7375ede04b9b8d565ca1",
+    "url": "/team-manager/static/js/1.7375ede0.chunk.js"
+  },
+  {
+    "revision": "41b21d2929a87aa299c1",
+    "url": "/team-manager/static/css/main.b25e862d.chunk.css"
+  },
+  {
+    "revision": "861cd39728e2e976716023a58535d121",
+    "url": "/team-manager/index.html"
+  }
+];
